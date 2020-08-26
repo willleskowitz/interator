ad9711c32dd7f1317fd3daae143a2c394df2e784
Will Leskowitz <59721872+willleskowitz@users.noreply.github.com>
2020-08-26 15:31:09 -0500

    Updated to include nth_fibonacci

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@
 from interator import (prime_stream, composite_stream, polygonal_stream,
                        fibonacci_stream, pell_stream, is_prime, miller_rabin,
                        is_composite, is_polygonal, is_fibonacci, is_pell,
-                       negafibonacci_stream)
+                       negafibonacci_stream, nth_fibonacci)

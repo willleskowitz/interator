8d89958eec3aa8de4884942a934a39081f5cb33a
Will Leskowitz <59721872+willleskowitz@users.noreply.github.com>
2020-09-01 14:17:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
- #!/usr/bin/env python
+#!/usr/bin/env python
 """Setup file for interator module."""
 
 import setuptools
 
 setuptools.setup(
-    name='interator_willleskowitz',
+    name='interator',
     version='0.1',
     author="Will Leskowitz",
     author_email="willleskowitz@gmail.com",
     packages=setuptools.find_packages(),
-    url='https://github.com/willleskowitz/interator/',
-    description='Integer sequence generation and related functions for conditional statements.',
+    url='https://github.com/willleskowitz/interator',
+    description='Integer sequence generation and related conditional tests.',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    keywords='integer sequence generator conditional tests',
+    keywords=['integer', 'sequence', 'generator', 'Fibonacci', 'Lucas',
+              'generalizations', 'prime', 'numbers', 'tests'],
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7'
+    python_requires='>=3.7',
+    setup_requires=['wheel'],
+    install_requires=['numpy']
 )

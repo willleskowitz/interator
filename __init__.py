9f2a9a0bac57a57c3f8aa98e0081f4aa40870e22
Will Leskowitz <59721872+willleskowitz@users.noreply.github.com>
2020-09-01 14:11:52 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@
 """__init__ file for interator module."""
 
 from interator import (prime_stream, composite_stream, polygonal_stream,
-                       fibonacci_stream, pell_stream, is_prime, miller_rabin,
-                       is_composite, is_polygonal, is_fibonacci, is_pell,
-                       negafibonacci_stream, nth_fibonacci)
+                       fibonacci_stream, negafibonacci_stream, lucas_stream,
+                       is_prime, miller_rabin, is_composite, is_polygonal,
+                       is_fibonacci, is_lucas, nth_fibonacci)
